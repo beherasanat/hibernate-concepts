2f0bb07d0bae5a7762bc0d557cd3f89d5c3df89e
sanat kumar behera <behera.sanat@gmail.com>
2024-07-16 15:51:16 +0530

    initial commit

---
+++ hib_jpa_test/src/main/java/com/hibtest/hib_jpa_test/HibJpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.hibtest.hib_jpa_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibJpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibJpaTestApplication.class, args);
+	}
+
+}
---
+++ hib_jpa_test/src/test/java/com/hibtest/hib_jpa_test/HibJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibtest.hib_jpa_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibJpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
